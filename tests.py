f1697f4201a2df884c7860361f4356574257c42f
Ismail Aksoy <iaksoy@seanapse.io>
2019-01-15 07:18:17 +0300

    start unittesting

---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+
+from peewee import *
+
+from task import Task
+
+
+MODELS = [Task]
+
+# use an in-memory SQLite for tests.
+test_db = SqliteDatabase(':memory:')
+
+
+class TaskTestCase(unittest.TestCase):
+    def setUp(self):
+        # Bind model classes to test db. Since we have a complete list of
+        # all models, we do not need to recursively bind dependencies.
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+    def tearDown(self):
+        # Not strictly necessary since SQLite in-memory databases only live
+        # for the duration of the connection, and in the next step we close
+        # the connection...but a good practice all the same.
+        test_db.drop_tables(MODELS)
+
+        # Close connection to db.
+        test_db.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
